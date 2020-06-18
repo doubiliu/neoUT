fb3d80b64be368a99c58e6a9569c7e85c9a9eea2
Luchuan <luchuan@ngd.neo.org>
2020-06-18 18:36:00 +0800

    Update src/neo/SmartContract/Native/Oracle/RequestStatusType.cs

--- src/neo/SmartContract/Native/Oracle/RequestStatusType.cs
+++ src/neo/SmartContract/Native/Oracle/RequestStatusType.cs
@@ -3,7 +3,7 @@ namespace Neo.SmartContract.Native.Tokens
     public enum RequestStatusType : byte
     {
         REQUEST = 0x00,
-        READY=0x01,
+        READY = 0x01,
         SUCCESSED=0x02,
         FAILED=0x03
     }
