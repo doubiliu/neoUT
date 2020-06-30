e04c4460a0e9b3e8a0bd64ec94ef785cbfb06991
Tommo-L <luchuan@neo.org>
2020-06-30 22:59:14 +0800

    remove policy file

--- src/neo/SmartContract/Native/Oracle/OracleContract.cs
+++ src/neo/SmartContract/Native/Oracle/OracleContract.cs
@@ -1,3 +1,4 @@
+using Neo.Cryptography.ECC;
 using Neo.IO;
 using Neo.Ledger;
 using Neo.Network.P2P.Payloads;
@@ -11,11 +12,14 @@ using Array = Neo.VM.Types.Array;
 
 namespace Neo.SmartContract.Native
 {
-    public partial class OracleContract : NativeContract
+    public class OracleContract : NativeContract
     {
         public override string Name => "Oracle";
         public override int Id => -5;
 
+        private const byte Prefix_Validator = 37;
+        private const byte Prefix_RequestBaseFee = 13;
+        private const byte Prefix_RequestMaxValidHeight = 33;
         private const byte Prefix_Request = 21;
         private const byte Prefix_Response = 27;
 
@@ -41,6 +45,68 @@ namespace Neo.SmartContract.Native
                 }
             };
             Manifest.Abi.Events = events.ToArray();
+        }
+
+        [ContractMethod(0_01000000, CallFlags.AllowStates)]
+        public bool SetOracleValidators(ApplicationEngine engine, ECPoint[] validators)
+        {
+            UInt160 committeeAddress = NativeContract.NEO.GetCommitteeAddress(engine.Snapshot);
+            if (!engine.CheckWitnessInternal(committeeAddress)) return false;
+            StorageKey key = CreateStorageKey(Prefix_Validator);
+            engine.Snapshot.Storages.GetAndChange(key, () => new StorageItem() { Value = validators.ToByteArray() });
+            return true;
+        }
+
+        [ContractMethod(0_01000000, CallFlags.AllowStates)]
+        public ECPoint[] GetOracleValidators(StoreView snapshot)
+        {
+            StorageKey key = CreateStorageKey(Prefix_Validator);
+            StorageItem item = snapshot.Storages.TryGet(key);
+            if (item is null) return NativeContract.NEO.GetCommittee(snapshot);
+            return item.Value.AsSerializableArray<ECPoint>();
+        }
+
+        public UInt160 GetOracleMultiSigAddress(StoreView snapshot)
+        {
+            ECPoint[] oracleValidators = GetOracleValidators(snapshot);
+            return Contract.CreateMultiSigContract(oracleValidators.Length - (oracleValidators.Length - 1) / 3, oracleValidators).ScriptHash;
+        }
+
+        [ContractMethod(0_03000000, CallFlags.AllowModifyStates)]
+        public bool SetRequestBaseFee(ApplicationEngine engine, long requestBaseFee)
+        {
+            UInt160 account = GetOracleMultiSigAddress(engine.Snapshot);
+            if (!engine.CheckWitnessInternal(account)) return false;
+            if (requestBaseFee <= 0) return false;
+            StorageItem storage = engine.Snapshot.Storages.GetAndChange(CreateStorageKey(Prefix_RequestBaseFee), () => new StorageItem());
+            storage.Value = BitConverter.GetBytes(requestBaseFee);
+            return true;
+        }
+
+        [ContractMethod(0_01000000, CallFlags.AllowStates)]
+        public long GetRequestBaseFee(StoreView snapshot)
+        {
+            StorageItem storage = snapshot.Storages.TryGet(CreateStorageKey(Prefix_RequestBaseFee));
+            if (storage is null) return 0;
+            return BitConverter.ToInt64(storage.Value);
+        }
+
+        [ContractMethod(0_03000000, CallFlags.AllowModifyStates)]
+        public bool SetRequestMaxValidHeight(ApplicationEngine engine, uint ValidHeight)
+        {
+            UInt160 account = GetOracleMultiSigAddress(engine.Snapshot);
+            if (!engine.CheckWitnessInternal(account)) return false;
+            StorageItem storage = engine.Snapshot.Storages.GetAndChange(CreateStorageKey(Prefix_RequestMaxValidHeight), () => new StorageItem());
+            storage.Value = BitConverter.GetBytes(ValidHeight);
+            return true;
+        }
+
+        [ContractMethod(0_01000000, CallFlags.AllowStates)]
+        public uint GetRequestMaxValidHeight(StoreView snapshot)
+        {
+            StorageItem storage = snapshot.Storages.TryGet(CreateStorageKey(Prefix_RequestMaxValidHeight));
+            if (storage is null) return 0;
+            return BitConverter.ToUInt32(storage.Value);
         }
 
         [ContractMethod(0_01000000, CallFlags.All)]
